c0bdf74db02c44d3a02b07879343e063f5a66951
Johnathan Sattler <jsattler@outlook.com>
2016-12-04 13:08:08 -0500

    Rewrote webgl code

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 var gl;
 
+var skyboxShader;
+var cubeShader;
+
+var uPMatrix = mat4.create();
+var uMVMatrix = [1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1];
+var uNMatrix = mat3.create();
+var invVT = mat3.create();
+
+var texture;
+var skybox;
+var centerCube;
+
+var xRot = 20.0, yRot = 0, zRot = 0;
+
 function initGL(canvas) {
 
     try {
@@ -12,6 +26,43 @@ function initGL(canvas) {
     if (!gl) {
         alert("Could not initialise WebGL, sorry :-(");
     }
+}
+
+function initShaders() {
+
+    // skybox shader
+    skyboxShader = createProgram("shader-fs", "shader-vs");
+
+    skyboxShader.positionAttribute =  gl.getAttribLocation(skyboxShader, "aPosition");
+    skyboxShader.mvMatrixUniform = gl.getUniformLocation(skyboxShader, "uMVMatrix");
+    skyboxShader.pMatrixUniform = gl.getUniformLocation(skyboxShader, "uPMatrix");
+
+    // cube shader
+    cubeShader = createProgram("cubeShader-fs", "cubeShader-vs");
+
+    cubeShader.positionAttribute =  gl.getAttribLocation(cubeShader, "aPosition");
+    cubeShader.normalAttribute =  gl.getAttribLocation(cubeShader, "aNormal");
+    cubeShader.mvMatrixUniform = gl.getUniformLocation(cubeShader, "uMVMatrix");
+    cubeShader.pMatrixUniform = gl.getUniformLocation(cubeShader, "uPMatrix");
+    cubeShader.nMatrixUniform = gl.getUniformLocation(cubeShader, "uNMatrix");
+    cubeShader.uInvVT = gl.getUniformLocation(cubeShader, "invVT");
+}
+
+function createProgram(fs, vs) {
+
+    var fragmentShader = getShader(gl, fs);
+    var vertexShader = getShader(gl, vs);
+
+    var program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+
+    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
+        alert("Could not initialise shaders");
+    }
+
+    return program;
 }
 
 function getShader(gl, id) {
@@ -53,105 +104,52 @@ function getShader(gl, id) {
     return shader;
 }
 
-var shaderProgram;
-var skyBoxShaderProgram;
-var cubeShaderProgram;
-
-function initShaders() {
-
-    skyBoxShaderProgram = createProgram("shader-fs", "shader-vs");
-    cubeShaderProgram = createProgram("cubeShader-fs", "cubeShader-vs");
-}
-
-function createProgram(fs, vs) {
-
-    var fragmentShader = getShader(gl, fs);
-    var vertexShader = getShader(gl, vs);
-
-    var program = gl.createProgram();
-    gl.attachShader(program, vertexShader);
-    gl.attachShader(program, fragmentShader);
-    gl.linkProgram(program);
-
-    if (!gl.getProgramParameter(program, gl.LINK_STATUS)) {
-        alert("Could not initialise shaders");
-    }
-
-    gl.useProgram(program);
-
-    program.vertexPositionAttribute = gl.getAttribLocation(program, "aVertexPosition");
-    gl.enableVertexAttribArray(program.vertexPositionAttribute);
-
-    program.textureCoordAttribute = gl.getAttribLocation(program, "aVertexNormal");
-    gl.enableVertexAttribArray(program.vertexNormalAttribute);
-
-    program.textureCoordAttribute = gl.getAttribLocation(program, "aTextureCoord");
-    gl.enableVertexAttribArray(program.textureCoordAttribute);
-
-    program.pMatrixUniform = gl.getUniformLocation(program, "uPMatrix");
-    program.mvMatrixUniform = gl.getUniformLocation(program, "uMVMatrix");
-    program.nMatrixUniform = gl.getUniformLocation(program, "uNMatrix");
-    program.samplerUniform = gl.getUniformLocation(program, "uSampler");
-
-    return program;
-}
-
-var mvMatrix = mat4.create();
-var mvMatrixStack = [];
-var pMatrix = mat4.create();
-var invVT = mat3.create();
-
-function mvPushMatrix() {
-
-    var copy = mat4.create();
-    mat4.set(mvMatrix, copy);
-    mvMatrixStack.push(copy);
-}
-
-function mvPopMatrix() {
-
-    if (mvMatrixStack.length == 0) {
-        throw "Invalid popMatrix!";
-    }
-    mvMatrix = mvMatrixStack.pop();
-}
-
-function setMatrixUniforms() {
-
-    gl.uniformMatrix4fv(shaderProgram.pMatrixUniform, false, pMatrix);
-    gl.uniformMatrix4fv(shaderProgram.mvMatrixUniform, false, mvMatrix);
-
-    var normalMatrix = mat3.create();
-    mat4.toInverseMat3(mvMatrix, normalMatrix);
-    mat3.transpose(normalMatrix);
-    gl.uniformMatrix3fv(shaderProgram.nMatrixUniform, false, normalMatrix);   
-}
-
-function degToRad(degrees) {
-
-    return degrees * Math.PI  / 180;
-}
-
-var skyboxVertexPositionBuffer;
-var skyboxVertexTextureCoordBuffer
-var skyboxVertexNormalBuffer;
-var skyboxVertexIndexBuffer;
-
-var cubeVertexPositionBuffer;
-var cubeVertexTextureCoordBuffer
-var cubeVertexNormalBuffer;
-var cubeVertexIndexBuffer;
-
 function initBuffers() {
 
-    initSkybox();
-    initCube();
+    initSkybox(1000);
+    initCube(20);
 }
 
-function initSkybox() {
+function initSkybox(size) {
 
-    skyboxVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, skyboxVertexPositionBuffer);
+    var modelData = cube(size);
+
+    skybox = {};
+
+    skybox.positionBuffer = gl.createBuffer();
+    skybox.indexBuffer = gl.createBuffer();
+    skybox.count = modelData.indices.length;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, skybox.positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, modelData.vertexPositions, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skybox.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, modelData.indices, gl.STATIC_DRAW);
+}
+
+function initCube(size) {
+
+    var modelData = cube(size);
+
+    centerCube = {};
+
+    centerCube.positionBuffer = gl.createBuffer();
+    centerCube.normalBuffer = gl.createBuffer();
+    centerCube.indexBuffer = gl.createBuffer();
+    centerCube.count = modelData.indices.length;
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, centerCube.positionBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, modelData.vertexPositions, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ARRAY_BUFFER, centerCube.normalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, modelData.vertexNormals, gl.STATIC_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, centerCube.indexBuffer);
+    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, modelData.indices, gl.STATIC_DRAW);
+}
+
+function cube(size) {
+
     var vertices = [
         // Front face
         -1.0, -1.0, 1.0,
@@ -190,165 +188,9 @@ function initSkybox() {
         -1.0,  1.0, -1.0
     ];
     for (var i = 0; i < vertices.length; i++) {
-        vertices[i] *= 10;
+        vertices[i] *= (size / 2);
     }
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    skyboxVertexPositionBuffer.itemSize = 3;
-    skyboxVertexPositionBuffer.numItems = 24;
 
-    skyboxVertexTextureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, skyboxVertexTextureCoordBuffer);
-    var textureCoords = [
-        // Front face
-        1.0,  0.34,
-        0.75, 0.34,
-        0.75, 0.66,
-        1.0,  0.66,
-
-        // Back face
-        0.25, 0.34, 
-        0.25, 0.66,
-        0.5,  0.66, 
-        0.5,  0.34,
-
-        // Top face
-        0.25, 0.66, 
-        0.25, 1.0, 
-        0.5,  1.0, 
-        0.5,  0.66, 
-
-        // Bottom face
-        0.25, 0.34, 
-        0.5,  0.34, 
-        0.5,  0.0, 
-        0.25, 0.0, 
-
-        // Right face
-        0.5,  0.34, 
-        0.5,  0.66, 
-        0.75, 0.66, 
-        0.75, 0.34, 
-
-        // Left face
-        0.25, 0.34, 
-        0.0,  0.34, 
-        0.0,  0.66, 
-        0.25, 0.66
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-    skyboxVertexTextureCoordBuffer.itemSize = 2;
-    skyboxVertexTextureCoordBuffer.numItems = 24;
-
-    skyboxVertexIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skyboxVertexIndexBuffer);
-    var cubeVertexIndices = [
-         0,  1,  2,      0,  2,  3, // Front face
-         4,  5,  6,      4,  6,  7, // Back face
-         8,  9, 10,      8, 10, 11, // Top face
-        12, 13, 14,     12, 14, 15, // Bottom face
-        16, 17, 18,     16, 18, 19, // Right face
-        20, 21, 22,     20, 22, 23  // Left face
-    ];
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-    skyboxVertexIndexBuffer.itemSize = 1;
-    skyboxVertexIndexBuffer.numItems = 36;
-
-    initCube();
-}
-
-function initCube() {
-
-    cubeVertexPositionBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-    var vertices = [
-        // Front face
-        -1.0, -1.0, 1.0,
-         1.0, -1.0, 1.0, 
-         1.0,  1.0, 1.0,
-        -1.0,  1.0, 1.0,
-
-         // Back face
-        -1.0, -1.0, -1.0,
-        -1.0,  1.0, -1.0,
-         1.0,  1.0, -1.0,
-         1.0, -1.0, -1.0,
-
-         // Top face
-        -1.0, 1.0, -1.0,
-        -1.0, 1.0,  1.0,
-         1.0, 1.0,  1.0,
-         1.0, 1.0, -1.0,
-
-         // Bottom face
-        -1.0, -1.0, -1.0,
-         1.0, -1.0, -1.0,
-         1.0, -1.0,  1.0,
-        -1.0, -1.0,  1.0,
-
-        // Right face
-        1.0, -1.0, -1.0,
-        1.0,  1.0, -1.0,
-        1.0,  1.0,  1.0,
-        1.0, -1.0,  1.0,
-
-        // Left fae
-        -1.0, -1.0, -1.0,
-        -1.0, -1.0,  1.0,
-        -1.0,  1.0,  1.0,
-        -1.0,  1.0, -1.0
-    ];
-    for (var i = 0; i < vertices.length; i++) {
-        vertices[i] *= 1.0;
-    }
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-    cubeVertexPositionBuffer.itemSize = 3;
-    cubeVertexPositionBuffer.numItems = 24;
-
-    cubeVertexTextureCoordBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-    var textureCoords = [
-        // Front face
-        1.0,  0.34,
-        0.75, 0.34,
-        0.75, 0.66,
-        1.0,  0.66,
-
-        // Back face
-        0.25, 0.34, 
-        0.25, 0.66,
-        0.5,  0.66, 
-        0.5,  0.34,
-
-        // Top face
-        0.25, 0.66, 
-        0.25, 1.0, 
-        0.5,  1.0, 
-        0.5,  0.66, 
-
-        // Bottom face
-        0.25, 0.0, 
-        0.5,  0.0, 
-        0.5,  0.34, 
-        0.25, 0.34, 
-
-        // Right face
-        0.5,  0.34, 
-        0.5,  0.66, 
-        0.75, 0.66, 
-        0.75, 0.34, 
-
-        // Left face
-        0.25, 0.34, 
-        0.0,  0.34, 
-        0.0,  0.66, 
-        0.25, 0.66
-    ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(textureCoords), gl.STATIC_DRAW);
-    cubeVertexTextureCoordBuffer.itemSize = 2;
-    cubeVertexTextureCoordBuffer.numItems = 24;
-
-    cubeVertexNormalBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexNormalBuffer);
     var vertexNormals = [
         // Front face
         0.0,  0.0,  1.0,
@@ -386,12 +228,45 @@ function initCube() {
         -1.0,  0.0,  0.0,
         -1.0,  0.0,  0.0,
     ];
-    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertexNormals), gl.STATIC_DRAW);
-    cubeVertexNormalBuffer.itemSize = 3;
-    cubeVertexNormalBuffer.numItems = 24;
 
-    cubeVertexIndexBuffer = gl.createBuffer();
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
+    var textureCoords = [
+        // Front face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0, 
+
+        // Back face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0, 
+
+        // Top face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0, 
+
+        // Bottom face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0, 
+
+        // Right face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0, 
+
+        // Left face
+        0.0, 0.0,
+        1.0, 0.0,
+        1.0, 1.0,
+        0.0, 1.0
+    ];
+
     var cubeVertexIndices = [
          0,  1,  2,      0,  2,  3, // Front face
          4,  5,  6,      4,  6,  7, // Back face
@@ -400,105 +275,131 @@ function initCube() {
         16, 17, 18,     16, 18, 19, // Right face
         20, 21, 22,     20, 22, 23  // Left face
     ];
-    gl.bufferData(gl.ELEMENT_ARRAY_BUFFER, new Uint16Array(cubeVertexIndices), gl.STATIC_DRAW);
-    cubeVertexIndexBuffer.itemSize = 1;
-    cubeVertexIndexBuffer.numItems = 36;
-}
 
-function handleLoadedTexture(texture) {
-
-    gl.bindTexture(gl.TEXTURE_2D, texture);
-
-    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
-    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, texture.image);
-
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
-    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
-
-    gl.bindTexture(gl.TEXTURE_2D, null);
-}
-
-var textureImage;
-
-function initTextures() {
-
-    textureImage = gl.createTexture();
-    textureImage.image = new Image();
-
-    textureImage.image.onload = function() {
-        handleLoadedTexture(textureImage)
+    return {
+        vertexPositions: new Float32Array(vertices),
+        vertexNormals: new Float32Array(vertexNormals),
+        vertexTextureCoords: new Float32Array(textureCoords),
+        indices: new Uint16Array(cubeVertexIndices)
     }
-
-    textureImage.image.src = "image.gif";
 }
 
-var xRot = 10;
-var yRot = 0;
-var zRot = 0;
+function initTextures(urls) {
 
-function drawScene() {
+    var ct = 0;
+    var img = new Array(6);
+    var urls = [
+       "cubemap/px.png", "cubemap/nx.png", 
+       "cubemap/py.png", "cubemap/ny.png", 
+       "cubemap/pz.png", "cubemap/nz.png"
+    ];
+    for (var i = 0; i < 6; i++) {
+        img[i] = new Image();
+        img[i].onload = function() {
+            ct++;
+            if (ct == 6) {
+                texture = gl.createTexture();
+                gl.bindTexture(gl.TEXTURE_CUBE_MAP, texture);
+                var targets = [
+                   gl.TEXTURE_CUBE_MAP_POSITIVE_X, gl.TEXTURE_CUBE_MAP_NEGATIVE_X, 
+                   gl.TEXTURE_CUBE_MAP_POSITIVE_Y, gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, 
+                   gl.TEXTURE_CUBE_MAP_POSITIVE_Z, gl.TEXTURE_CUBE_MAP_NEGATIVE_Z 
+                ];
+                for (var j = 0; j < 6; j++) {
+                    gl.texImage2D(targets[j], 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img[j]);
+                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+                    gl.texParameteri(gl.TEXTURE_CUBE_MAP, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+                }
+                gl.generateMipmap(gl.TEXTURE_CUBE_MAP);
+                tick();
+            }
+        }
+        img[i].src = urls[i];
+    }
+}
 
-    gl.viewport(0, 0, gl.viewportWidth, gl.viewportHeight);
+function draw() {
+
+    gl.clearColor(0,0,0,1);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-    mat4.perspective(45, gl.viewportWidth / gl.viewportHeight, 0.1, 100.0, pMatrix);
+    mat4.perspective(uPMatrix, Math.PI/3, gl.viewportWidth / gl.viewportHeight, gl.viewportWidth / gl.viewportHeight, 2000);
 
-    // render skybox
-    drawSkyBox();
+    mat4.identity(uMVMatrix);
+    mat4.translate(uMVMatrix, uMVMatrix, [0, 0.0, -40.0]);
 
-    // render cube
-    drawSmallCube();
+    mat4.rotateX(uMVMatrix, uMVMatrix, degToRad(xRot));
+    mat4.rotateY(uMVMatrix, uMVMatrix, degToRad(yRot));
+    mat4.rotateZ(uMVMatrix, uMVMatrix, degToRad(zRot));
+
+    mat3.normalFromMat4(uNMatrix, uMVMatrix);
+
+    // Draw the skybox
+    drawSkybox();
+
+    // Draw the cube.
+    drawCube();
 }
 
-function drawSkyBox() {
+function drawSkybox() {
 
-    shaderProgram = skyBoxShaderProgram;
-    gl.useProgram(skyBoxShaderProgram);
+    gl.useProgram(skyboxShader);
 
-    mat4.identity(mvMatrix);
-    mat4.translate(mvMatrix, [0, 0.0, -7.5]);
+    gl.uniformMatrix4fv(skyboxShader.pMatrixUniform, false, uPMatrix);
 
-    mat4.rotate(mvMatrix, degToRad(xRot), [1, 0, 0]);
-    mat4.rotate(mvMatrix, degToRad(yRot), [0, 1, 0]);
-    mat4.rotate(mvMatrix, degToRad(zRot), [0, 0, 1]);
+    if (texture) {
+        gl.enableVertexAttribArray(skyboxShader.positionAttribute);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, skyboxVertexPositionBuffer);
-    gl.vertexAttribPointer(shaderProgram.vertexPositionAttribute, skyboxVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, skybox.positionBuffer);
+        gl.vertexAttribPointer(skyboxShader.positionAttribute, 3, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, skyboxVertexTextureCoordBuffer);
-    gl.vertexAttribPointer(shaderProgram.textureCoordAttribute, skyboxVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.uniformMatrix4fv(skyboxShader.mvMatrixUniform, false, uMVMatrix);
 
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, textureImage);
-    gl.uniform1i(shaderProgram.samplerUniform, 0);
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skybox.indexBuffer);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, skyboxVertexIndexBuffer);
-    setMatrixUniforms();
-    gl.drawElements(gl.TRIANGLES, skyboxVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+        gl.drawElements(gl.TRIANGLES, skybox.count, gl.UNSIGNED_SHORT, 0);
+
+        gl.disableVertexAttribArray(skyboxShader.positionAttribute);
+    }
+
+    mat3.fromMat4(invVT, uMVMatrix);
+    mat3.invert(invVT,invVT);
+
+    mat3.normalFromMat4(uNMatrix, uMVMatrix);
 }
 
-function drawSmallCube() {
+function drawCube() {
 
-    shaderProgram = cubeShaderProgram;
-    gl.useProgram(cubeShaderProgram);
+    gl.useProgram(cubeShader);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexPositionBuffer);
-    gl.vertexAttribPointer(cubeShaderProgram.vertexPositionAttribute, cubeVertexPositionBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    gl.uniformMatrix4fv(cubeShader.pMatrixUniform, false, uPMatrix);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexNormalBuffer);
-    gl.vertexAttribPointer(cubeShaderProgram.vertexNormalAttribute, cubeVertexNormalBuffer.itemSize, gl.FLOAT, false, 0, 0);
+    if (texture) {
+        gl.enableVertexAttribArray(cubeShader.positionAttribute);
+        gl.enableVertexAttribArray(cubeShader.normalAttribute);
 
-    gl.bindBuffer(gl.ARRAY_BUFFER, cubeVertexTextureCoordBuffer);
-    gl.vertexAttribPointer(cubeShaderProgram.textureCoordAttribute, cubeVertexTextureCoordBuffer.itemSize, gl.FLOAT, false, 0, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, centerCube.positionBuffer);
+        gl.vertexAttribPointer(cubeShader.positionAttribute, 3, gl.FLOAT, false, 0, 0);
 
-    gl.activeTexture(gl.TEXTURE0);
-    gl.bindTexture(gl.TEXTURE_2D, textureImage);
-    gl.uniform1i(cubeShaderProgram.samplerUniform, 0);
+        gl.bindBuffer(gl.ARRAY_BUFFER, centerCube.normalBuffer);
+        gl.vertexAttribPointer(cubeShader.normalAttribute, 3, gl.FLOAT, false, 0, 0);
 
-    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, cubeVertexIndexBuffer);
-    setMatrixUniforms();
-    gl.drawElements(gl.TRIANGLES, cubeVertexIndexBuffer.numItems, gl.UNSIGNED_SHORT, 0);
+        gl.uniformMatrix4fv(cubeShader.mvMatrixUniform, false, uMVMatrix);
+        gl.uniformMatrix3fv(cubeShader.nMatrixUniform, false, uNMatrix);
+        gl.uniformMatrix3fv(cubeShader.uInvVT, false, invVT);
+
+        gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, centerCube.indexBuffer);
+
+        gl.drawElements(gl.TRIANGLES, centerCube.count, gl.UNSIGNED_SHORT, 0);  
+
+        gl.disableVertexAttribArray(cubeShader.positionAttribute);
+        gl.disableVertexAttribArray(cubeShader.normalAttribute);
+    }
+}
+
+function degToRad(degrees) {
+
+    return degrees * Math.PI  / 180;
 }
 
 var lastTime = 0;
@@ -511,7 +412,7 @@ function animate() {
         var elapsed = timeNow - lastTime;
 
         //xRot += (90 * elapsed) / 1000.0;
-        yRot += (90 * elapsed) / 1000.0 / 5;
+        yRot += (90 * elapsed) / 1000.0 / 10.0;
         //zRot += (90 * elapsed) / 1000.0;
     }
 
@@ -520,8 +421,8 @@ function animate() {
 
 function tick() {
 
-    requestAnimFrame(tick);
-    drawScene();
+    requestAnimationFrame(tick);
+    draw();
     animate();
 }
 
